e54553115a60d2cadcf3f94cec19bc2d4e24f374
Hemalathad80 <hemalathad80@gmail.com>
2021-07-08 14:13:08 +1000

    modified

--- src/com/MyBank/Account/CheckingAccountTest.java
+++ src/com/MyBank/Account/CheckingAccountTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.MyBank.InsufficientFundsException;
+
 class CheckingAccountTest {
 	// class under test
 	CheckingAccount account;
@@ -40,25 +42,71 @@ class CheckingAccountTest {
 
 		// bad amount
 		double amount = -1.0;
-		
 
 		assertThrows(IllegalArgumentException.class, () -> {
 			account.deposit(amount);
 		});
 
 		// if we get here,the test failed
-		//fail("Expected a throw,but deposit did not throw");
+		// fail("Expected a throw,but deposit did not throw");
 	}
-	
+
 	@Test
 	void deposit__amount_equal_to_zero__throws() {
-		//another bad amount
+		// another bad amount
 		double amount = 0.0;
-		
+
 		assertThrows(IllegalArgumentException.class, () -> {
 			account.deposit(amount);
 		});
+
+	}
+
+	@Test
+	void withdraw__amount_less_than_balance__works() throws InsufficientFundsException {
+
+		double amount = 30.0;
 		
+
+		assertThrows(InsufficientFundsException.class, () -> {
+			account.withdraw(amount);
+		});
+
+		assertTrue(account.getBalance() > amount, "You can withdraw");
+
+	}
+
+	@Test
+	void withdraw__amount_greater_than_balance() throws
+
+	InsufficientFundsException {
+
+		double amount = 100.0;
+		double balance = account.getBalance();
+		// account.withdraw(amount);
+
+		assertThrows(InsufficientFundsException.class, () -> {
+			account.withdraw(amount);
+		});
+
+		assertTrue(amount > balance, "You cannot withdraw");
+
+	}
+
+	@Test
+	void withdraw__amount_equal_to_balance() {
+
+		double amount = 42.0;
+		double balance = account.getBalance();
+		// account.withdraw(amount);
+
+		
+		  assertThrows(IllegalArgumentException.class, () -> {
+		  account.withdraw(amount); });
+		 
+
+		assertTrue(amount == balance, "You can withdraw");
+
 	}
 
 }
